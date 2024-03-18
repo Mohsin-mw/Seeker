95445711a2ceab253fe90255281b7caabaad9e10
Mohsin <122507740+Mohsin-mw@users.noreply.github.com>
2024-03-18 22:13:44 +0500

    BREAKING-CHANGES: Postgres configured

--- server/seeker/urls.py
+++ server/seeker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for superio project.
+URL configuration for seeker project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,18 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from superio.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-
+from django.urls import path
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
